515aadf9eafc09faea9e5f5fc10b2e9afe4f9d7e
user <user@full368-air.local>
2017-07-03 14:20:00 +0530

    Used navigation controller and created a signUp page.

--- PassingData/SecondViewController.swift
+++ PassingData/SecondViewController.swift
@@ -9,26 +9,43 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    @IBOutlet weak var incomingtextlabel: UILabel!
     
-    var incomingText : String?
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var email_id: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        incomingtextlabel.text = incomingText
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func emailValidation(email:String) -> Bool {
+        let predicate = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", predicate)
+        return emailTest.evaluate(with: email)
     }
-    */
+    
+    let storyboar : UIStoryboard =  UIStoryboard(name: "Main", bundle: nil)
+    
+    @IBAction func signup(_ sender: Any) {
+        
+        let isemailValidated = emailValidation(email: email_id.text!)
+        
+        let nextViewController = self.storyboar.instantiateViewController(withIdentifier: "WelcomeViewController") as! WelcomeViewController
+        
+        if (userName.text?.isEmpty)! || (email_id.text?.isEmpty)! || (password.text?.isEmpty)! {
+           print("One of the field is missing")
+            
+        } else if (isemailValidated == false) || (password.text?.characters.count)! < 6 {
+            print("Please provide valid details")
 
+        } else {
+            self.present(nextViewController, animated: true, completion: nil)
+            //self.navigationController?.pushViewController(nextViewController, animated: true)
+
+        }
+    }
+   
 }
---
+++ PassingData/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  PassingData
+//
+//  Created by user on 7/2/17.
+//  Copyright © 2017 full. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
